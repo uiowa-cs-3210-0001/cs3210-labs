271df6d7ed9aa6009264b41cb6c91199ba6bfceb
Aleksey Gurtovoy <agurtovoy@acm.org>
2019-03-04 17:15:34 -0600

    Lab13 examples

---
+++ lab13/0 - vector.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+struct Entry
+{
+    string name;
+    string phone_number;
+};
+
+ostream& operator<<( ostream& out, Entry const& e )
+{
+    return out << e.name << ": " << e.phone_number;
+}
+
+
+int main()
+{
+    vector<Entry> phone_book = {
+        { "David Hume", "1-319-515-0217" },
+        { "Karl Popper", "1-818-921-3650" },
+        { "1-800-Flowers", "1-800-716-4851" },
+    };
+
+    for ( unsigned i = 0; i < phone_book.size(); ++i )
+        cout << phone_book[i] << endl;
+}
